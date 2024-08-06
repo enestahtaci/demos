6a613de93dd96619f9a971ab43e2ea046c506c15
Enes <enestahtaci03@gmail.com>
2024-08-06 11:58:09 +0300

    Initial commit

---
+++ voltdb_evrencell/src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.voltdb.client.Client;
+import org.voltdb.client.ClientConfig;
+import org.voltdb.client.ClientFactory;
+import org.voltdb.client.ClientResponse;
+import org.voltdb.VoltTable;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+            ClientConfig clientConfig = new ClientConfig();
+            Client client = ClientFactory.createClient(clientConfig);
+
+            client.createConnection("34.132.243.222:32776");
+
+            ClientResponse response = client.callProcedure("PROC3");
+
+            if (response.getStatus() == ClientResponse.SUCCESS) {
+                VoltTable results = response.getResults()[0];
+                while (results.advanceRow()) {
+                    System.out.println("ID: " + results.getLong("ID") +
+                            ", NAME: " + results.getString("NAME"));
+                }
+            } else {
+                System.out.println("Procedure call failed: " + response.getStatusString());
+            }
+
+            client.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
